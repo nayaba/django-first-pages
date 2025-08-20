70dec9a08efd06933b24428d3f33f2f5ba47fc86
Nabila Ayaba <nabilaayaba@gmail.com>
2025-08-20 10:34:15 +0300

    Create home page url, view, and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  
+  context = {
+    "title": "My App"
+  }
+
+  return render(request, 'home.html', context)
