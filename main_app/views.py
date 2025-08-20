1918e4c0b4d7867b2e130a824bca79e791c5b0f1
Nabila Ayaba <nabilaayaba@gmail.com>
2025-08-20 11:00:38 +0300

    Add about page and css

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-  
   context = {
     "title": "My App"
   }
-
   return render(request, 'home.html', context)
+
+
+def about(request):
+  return render(request, 'about.html')
